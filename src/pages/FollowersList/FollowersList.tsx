f8a8e006b0d43ed4c5adfaab49bf15538bd582b9
Dhruv Solanki <95128593+dhruvsolanki0811@users.noreply.github.com>
2024-03-13 18:45:20 +0530

    Added Search Functionalities in Connections page

--- src/pages/FollowersList/FollowersList.tsx
+++ src/pages/FollowersList/FollowersList.tsx
@@ -13,6 +13,8 @@ import {
   useHandleRejection,
 } from "../../hooks/useConnectionsData";
 import { useUserAuthStore } from "../../store/AuthStore";
+import { CiSearch } from "react-icons/ci";
+import { useState } from "react";
 
 function FollowersList() {
   let { page } = useParams();
@@ -23,13 +25,26 @@ function FollowersList() {
   const { mutate: handleReject, isLoading: handleRejectionLoading } =
     useHandleRejection(userName);
   const pageType = page ? page : "";
-  const {
-    data: connections,
-    isLoading: loader
-  } = useFetchUsersConnections(pageType, userName);
+  const { data: connections, isLoading: loader } = useFetchUsersConnections(
+    pageType,
+    userName
+  );
 
   const navigate = useNavigate();
 
+  const [search, setSearch] = useState("");
+
+  // Handler for search input change
+  const handleSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+  const filteredConnections = connections
+    ? connections.filter((connection) => {
+        const fullName =
+          `${connection.firstname} ${connection.lastname}`.toLowerCase();
+        return fullName.includes(search.toLowerCase());
+      })
+    : [];
   return (
     <>
       <div className="main-wrapper">
@@ -48,12 +63,6 @@ function FollowersList() {
               >
                 Connections
               </div>
-              {/* <div
-                  onClick={() => navigate("/connections/pending")}
-                  className={`jobtype-container cursor-pointer primary-text flex justify-center ${page=='pending'&&"all-section"}`} 
-                >
-                  Pending Requests
-                </div> */}
               <div
                 onClick={() => navigate("/connections/received")}
                 className={`jobtype-container cursor-pointer  primary-text flex justify-center items-center h-full ${
@@ -63,19 +72,32 @@ function FollowersList() {
                 Connections Requests
               </div>
             </div>
+            <div className="search-section w-full h-19 ps-5 pt-3 pb-3 pe-5  ">
+              <div className="input-search-container w-full flex justify-center items-center roundedfull ps-1 pe-1 ">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="search-box"
+                  value={search}
+                  onChange={handleSearchInput}
+                  // onChange={handleSearchInput}
+                  // onKeyDown={handleEnter}
+                />
+                <CiSearch className="cursor-pointer"></CiSearch>
+              </div>
+            </div>
           </div>
           {loader || handleConnectionLoading || handleRejectionLoading ? (
             <Loader></Loader>
           ) : (
             <div className="people-grid scrollable-content max-sm:mb-[3.9rem]  flex p-3  w-full ">
               {connections &&
-                connections.map((user: any,key) => (
+                filteredConnections.map((user: any, key) => (
                   <div key={key}>
                     <div className="people-box flex flex-col  ps-3 pe-3">
                       <div className="follow-container flex gap-3 items-center">
                         <div className="profile-pic flex justify-center items-center  h-[2.8rem] w-[3rem] mt-2 overflow-hidden border-[1px] rounded-full">
-                          {user?.profile_pic == null 
-                          ? (
+                          {user?.profile_pic == null ? (
                             <img
                               className=" object-  h-[70%] w-[70%]  "
                               src={placeHolder}
@@ -134,7 +156,8 @@ function FollowersList() {
                         </div>
                       </div>
                       <div className="people-username text-[14px] font-medium mt-1 flex gap-2">
-                        {user.firstname}{user.lastname} {" "} @{user.username}
+                        {user.firstname}
+                        {user.lastname} @{user.username}
                       </div>
                       <div className="text-three-line color-lgt-grey w-full text-[11px] pe-4 mb-1">
                         {user.description}
